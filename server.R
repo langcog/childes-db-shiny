b47a4581526ff8cbbf36fdd9f8b7182cdf47d790
Michael C. Frank <mcfrank@stanford.edu>
2017-08-21 14:02:07 -0700

    more functionality

--- server.R
+++ server.R
@@ -2,8 +2,10 @@ library(shiny)
 library(tidyverse)
 library(feather)
 library(ggthemes)
+library(DT)
 
-adam <- read_feather("adam.feather")
+# adam <- read_feather("adam.feather")
+brown <- read_feather("brown_utts.feather")
 
 DAYS_PER_YEAR <- 365.25
 DAYS_PER_MONTH <- DAYS_PER_YEAR / 12
@@ -11,19 +13,44 @@ DAYS_PER_MONTH <- DAYS_PER_YEAR / 12
 # MAIN SHINY SERVER
 server <- function(input, output, session) {
   
-  # ------- PULL DATA -------
-  utt_tbl <- reactive({
-    adam
+  # --------------------- DATA ---------------------
+  data <- reactive({
+    brown 
   })
   
-  # ------- UI ELEMENTS -------
+  # KIDS IN DATA
+  children <- reactive({
+    data() %>%
+      pull(target_child_name) %>%
+      unique
+  })
+  
+  # # UTTERANCES
+  # utts <- reactive({
+  #   print("utts")
+  #   print(input$target_children)
+  #   data() %>%
+  #     filter()
+  # })
   
   # ROLES USED IN DATA
   roles <- reactive({
-    utt_tbl() %>%
+    data() %>%
       pull(speaker_role) %>%
       unique
   })
+  
+  # --------------------- UI ELEMENTS ---------------------
+ 
+  # SELECTOR FOR KIDS
+  output$children_selector <- renderUI({
+    selectizeInput(inputId = "target_children",
+                   label = "Target Child (Corpus)", 
+                   choices = children(), 
+                   selected = "Adam", 
+                   multiple = TRUE)
+  })
+  
   
   # SELECTOR FOR ROLES
   output$role_selector <- renderUI({
@@ -35,33 +62,37 @@ server <- function(input, output, session) {
   })
   
   
-  # ------- DATA -------
+  # --------------------- COMPUTATION OF MLUS ---------------------
+  
   
   # COMPUTE MLUS
   mlus <- reactive({
-    utt_tbl() %>%
-      filter(speaker_role %in% input$roles,
+    data() %>%
+      filter(target_child_name %in% input$target_children,
+             speaker_role %in% input$roles,
              target_child_age >= input$age_range[1] * DAYS_PER_YEAR,
              target_child_age <= input$age_range[2] * DAYS_PER_YEAR) %>%
       mutate(age_mo = target_child_age / DAYS_PER_MONTH, 
              age_mo_binned = floor(age_mo / input$age_binwidth) * input$age_binwidth, 
              age_y = (age_mo_binned + input$age_binwidth/2)/12) %>%
-      group_by(speaker_role, age_y) %>%
-      summarise(mlu = mean(length),
-                n = n())
+      group_by(target_child_name, speaker_role, age_y) %>%
+      summarise(mlu = signif(mean(length), digits = 2),
+                n = n()) %>%
+      ungroup
   })
   
-  # ------- DISPLAY ELEMENTS -------
+  # --------------------- DISPLAY ---------------------
+  
   
   # TRAJECTORY
   output$trajectory_plot <- renderPlot({
     ggplot(mlus(), 
            aes(x = age_y,
                y = mlu, 
-               col = speaker_role,
-               group = speaker_role)) +
+               col = speaker_role)) +
       geom_point(aes(size = n)) +
-      geom_smooth() + 
+      geom_smooth(se=FALSE) + 
+      facet_wrap(~target_child_name) +
       ylab("Mean Length of Utterance") + 
       xlab("Target Child Age (years)") + 
       ylim(0, ceiling(max(mlus()$mlu))) +
@@ -69,6 +100,10 @@ server <- function(input, output, session) {
       scale_colour_solarized(name = "Speaker Role") + 
       scale_size_continuous(name = "Number of Utterances") + 
       theme_few() +
-      theme(legend.position = "Bottom") 
+      theme(legend.position = "bottom") 
+  })
+  
+  output$trajectory_table <- renderDataTable({
+    datatable(mlus())
   })
 }
--- ui.R
+++ ui.R
@@ -1,10 +1,14 @@
 library(shiny)
+library(shinyBS)
+library(shinythemes)
+
 
 ui <- fluidPage(
   titlePanel("Mean Length of Utterance"),
   
   sidebarLayout(
     sidebarPanel(
+      uiOutput("children_selector"),
       uiOutput("role_selector"),
       sliderInput("age_range", 
                   label="Ages to include (years)", 
@@ -17,7 +21,14 @@ ui <- fluidPage(
     ),
     
     mainPanel(
-      plotOutput("trajectory_plot")
+      tabsetPanel(selected = "Plot", 
+        tabPanel("Plot",
+                 plotOutput("trajectory_plot")
+        ), 
+        tabPanel("Table",
+                 dataTableOutput("trajectory_table")
+        )
+      )
     )
   )
 )
